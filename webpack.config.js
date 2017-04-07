27a150601c10da3b088e16905b369ec63e991a3a
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-04-07 13:29:08 +1200

    * Upgraded to Webpack 2, stripped everything, installed bulma

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
-var autoprefixer = require('autoprefixer');
-var precss       = require('precss');
-var webpack = require("webpack");
+const webpack = require("webpack");
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 	entry: './js/index.js',
 	output: {
-        path: './',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015']
-                }
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: "scripts.js",
+  },
+  module: {
+    rules: [
+        {
+            test: /.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+                presets: ['es2015']
             },
-            {
-                test:   /.css$/,
-                loader: 'style-loader!css-loader!postcss-loader'
-            }
-        ]
-    },
-    postcss: function () {
-        return [autoprefixer, precss];
-    },
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "sass-loader"
+              }
+            ],
+            // use style-loader in development
+            fallback: "style-loader"
+          }),
+        }
+    ]
+  },
+    plugins: [
+      new ExtractTextPlugin({
+          filename: "styles.css",
+      }),
+    ],
 }
